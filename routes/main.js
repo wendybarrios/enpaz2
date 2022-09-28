899e1fb268d3a469cded30bd68cfa152804a530d
wendy barrios <wbarrios45@yahoo.com>
2022-09-28 14:42:45 -0700

    Added about route and about.css

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const aboutController = require('../controllers/about')
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -14,5 +15,6 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get('/about', aboutController.getIndex)
 
 module.exports = router;
