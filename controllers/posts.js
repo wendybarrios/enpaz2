c56500281054009dc69dbc2031c23e9965b8c4fb
wendy barrios <wbarrios45@yahoo.com>
2023-01-04 12:25:09 -0800

    edited getFeed in posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   getFeed: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+      const posts = await Post.find({user:req.user.id}).sort({ createdAt: "desc" }).lean();
       res.render("feed.ejs", { posts: posts, user: req.user});
     } catch (err) {
       console.log(err);
