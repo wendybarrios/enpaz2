5ad13403f12cd5dd75e364e469af2d1b662e107b
wendy barrios <wbarrios45@yahoo.com>
2023-01-28 11:21:32 -0800

    added comments in posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -7,10 +7,11 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.get("/:id", ensureAuth, postsController.getPost);
 
+// enables user to create post w/ cloudinary for media upload
 router.post("/createPost", upload.single("file"), postsController.createPost);
-
+// enables user to like post 
 router.put("/likePost/:id", postsController.likePost);
-
+// enables user to delete post. In controller, uses POST model to delete post from MongoDB collection
 router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
